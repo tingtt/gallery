e708070220a7a4ba34bbdfe136c7aff1710450e0
taku-ting <ting.taku@gmail.com>
2025-01-12 10:16:42 +0900

    feat: add back button

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
 import UploadWithURL from "./_action/url_upload";
+import clsx from "clsx";
 
 export default function Upload(): JSX.Element {
   return (
-    <>
+    <div
+      className={clsx(
+        ["min-h-screen", "p-4"],
+        ["flex", "flex-col", "justify-between"]
+      )}
+    >
       {/* <form action="/upload/1" method="POST" enctype="multipart/form-data">
         <label>
           <input type="file" name="file" className="btn" />
@@ -10,10 +17,21 @@ export default function Upload(): JSX.Element {
         </label>
         <input type="submit" value="Submit" className="btn btn-primary" />
       </form> */}
-      <form action={UploadWithURL}>
-        <input type="text" name="url" className="input input-bordered" />
+      <form action={UploadWithURL} className={clsx("flex", "gap-2")}>
+        <input
+          type="text"
+          name="url"
+          required
+          className={clsx("input", "input-bordered")}
+        />
         <input type="submit" value="Submit" className="btn btn-primary" />
       </form>
-    </>
+      <Link
+        href={"/"}
+        className={clsx("btn", "btn-circle", "btn-ghost", "z-10")}
+      >
+        ←
+      </Link>
+    </div>
   );
 }
